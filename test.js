e0b4d06f67fcfdc5952bd1ac1e2bd74b1fc47885
Michael Van Der Velden <mikevdv@vandie.co.uk>
2017-03-16 14:44:18 +0000

    new swear words

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
 var isprofanity = require('./isProfanity.js');
-
-//var ta = ['Thanks for helping to solve the bug.','Thanks for helping me to create such a cool system :D',"That was a great game you played the other day.","Thanks for helping to test the thanks swear checker...","Thanks for helping to test thanks","RobinsonThomas Robinson was thanked by Joseph Crane \"thanks man.\" positive positive","Make Andy proud","Thanks for the holy water","Yay your on thanks!","Thanks for the hep testing thanks."];
-var ta =['Thanks for engineering that solution.','Go fu*k yourself.'];
+var ta =['gobshite'];
 
 ta.forEach(function(tstring) {
     isprofanity(tstring,function(t,a){
-        console.log(tstring+':'+t+':'+JSON.stringify(a));
+        g = t ? 'has profanity '+': Found '+JSON.stringify(a) : 'has no profanity \n';
+        console.log('"'+tstring+'" '+g);
     });
-}, this);
+});
